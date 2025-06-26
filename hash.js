039f96411d041effebeef77f990b1f8c70083334
bioroid17 <trace1997@ajou.ac.kr>
2025-06-26 16:42:07 +0900

    파일 추가

---
+++ hash.js
@@ -0,0 +1,24 @@
+class HashMap {
+  constructor() {
+    this.keys = [];
+    this.values = [];
+  }
+  clear() {}
+
+  containsKey(key) {}
+
+  get(key) {}
+
+  isEmpty() {}
+
+  keys() {}
+
+  put(key, value) {}
+
+  remove(key) {}
+
+  size() {}
+}
+
+let hashmap = new HashMap();
+console.log(hashmap);

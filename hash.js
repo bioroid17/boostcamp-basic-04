05e4c347a9aae08d502a63a5da1b3f0f1d46a751
bioroid17 <trace1997@ajou.ac.kr>
2025-06-26 18:28:00 +0900

    HashMap 완성

--- hash.js
+++ hash.js
@@ -1,24 +1,79 @@
 class HashMap {
   constructor() {
-    this.keys = [];
-    this.values = [];
+    this.keyList = [];
+    this.valueList = [];
   }
-  clear() {}
+  clear() {
+    this.keyList = [];
+    this.valueList = [];
+  }
 
-  containsKey(key) {}
+  containsKey(key) {
+    const index = this.keyList.indexOf(key);
+    if (index !== -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-  get(key) {}
+  get(key) {
+    if (this.containsKey(key)) {
+      const index = this.keyList.indexOf(key);
+      return this.valueList[index];
+    } else {
+      throw new Error("해당 key가 존재하지 않습니다.");
+    }
+  }
 
-  isEmpty() {}
+  isEmpty() {
+    return this.keyList.length === 0;
+  }
 
-  keys() {}
+  keys() {
+    return String(this.keyList);
+  }
 
-  put(key, value) {}
+  put(key, value) {
+    if (this.containsKey(key)) {
+      const index = this.keyList.indexOf(key);
+      this.valueList[index] = value;
+    } else {
+      this.keyList.push(key);
+      this.valueList.push(value);
+    }
+  }
 
-  remove(key) {}
+  remove(key) {
+    if (this.containsKey(key)) {
+      const index = this.keyList.indexOf(key);
+      const removedKey = this.keyList.splice(index, 1);
+      const removedValue = this.valueList.splice(index, 1);
+    }
+  }
 
-  size() {}
+  size() {
+    return this.keyList.length;
+  }
 }
 
-let hashmap = new HashMap();
+const hashmap = new HashMap();
 console.log(hashmap);
+console.log(hashmap.isEmpty()); // true
+hashmap.put("name", "Alice");
+hashmap.put("age", 30);
+console.log(hashmap);
+console.log(hashmap.isEmpty()); // false
+console.log(hashmap.size()); // 2
+console.log(hashmap.containsKey("age")); // true
+console.log(hashmap.get("age")); // 30
+console.log(hashmap.containsKey("name")); // true
+console.log(hashmap.get("name")); // Alice
+console.log(hashmap.keys()); // name,age
+hashmap.remove("age");
+console.log(hashmap.size()); // 1
+console.log(hashmap);
+console.log(hashmap.containsKey("age")); // false
+hashmap.clear();
+console.log(hashmap);
+console.log(hashmap.isEmpty()); // true
